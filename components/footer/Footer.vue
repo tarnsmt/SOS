64a3674886458a5c977d053b0760bf6a1db6652e
tarnsmt <tarnsmt@tarnsmts-MacBook-Pro.local>
2020-04-23 16:07:24 +0700

    Frontend nuxtjs

---
+++ components/footer/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="footer">
+    <div class="columns container">
+      <div class="column">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    <div class="column has-text-right">
+      <p>Shopping Online Project, 2020</p>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VmFooter',
+}
+</script>
+
+<style lang="scss" scoped>
+ .footer {
+   background: black;
+   color: white;
+ }
+</style>
---
+++ components/hero/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hero is-medium is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Shopping Online
+        </h1>
+        <h2 class="subtitle">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+        </h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+    .hero {
+      background-color: black;    
+
+    }
+    h1, h2 {
+      color: #fff;
+    }
+</style>
